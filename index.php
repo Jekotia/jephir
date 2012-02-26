4d24a21595c39b834e6fd8941a490a6785b0f4ba
Jekotia <ningleepo@gmail.com>
2012-02-26 00:59:24 -0500

    Fully planned out use of mod_rewrite and what page URL's will be for different things.

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!---
+single page
+	/blog						| index.php?page=blog
+categorized page
+	/user/login					| index.php?category=user&page=login
+category
+	/category/news				| index.php?category=news
+tag
+	/tag/jephir					| index.php?tag=jephir
+single post
+	/blog/2012/02/25/test-post	| index.php?page=blog&year=2012&month=02&date=25&name=test-post
+archive
+	/archive/2012/02			| index.php?page=archive&year=2012&month=02
+non-practical sample containing	all variables
+	index.php?page=blog&year=2012&month=02&date=25&name=test-post&category=news&tag=jephir
+--->
+
 <a href="login" >login</a>
 <a href="register" >register</a>
 <a href="logout" >logout</a>
@@ -6,7 +23,16 @@
 <a href="about" >about</a>
 <a href="contact" >contact</a>
 
-<!--- test.php?p=blog&archive=5  --->
+<?php
+	if (isset($_GET['page'])) echo $_GET['page'];
+	if (isset($_GET['year'])) echo $_GET['year'];
+	if (isset($_GET['month'])) echo $_GET['month'];
+	if (isset($_GET['date'])) echo $_GET['date'];
+	if (isset($_GET['name'])) echo $_GET['name'];
+	if (isset($_GET['category'])) echo $_GET['category'];
+	if (isset($_GET['tag'])) echo $_GET['tag'];
+?> 
+ 
 <br/>
 <br/>
 <?php
@@ -18,10 +44,10 @@
 	include('func/database.php');
 	include('inc/init.php');
 
-	if (isset($_GET['p']))
+	if (isset($_GET['page']))
 	{
 	// auth pages
-		$p = $_GET['p'];
+		$p = $_GET['page'];
 		if ($p == 'login')
 		{
 			include('inc/login.php');
