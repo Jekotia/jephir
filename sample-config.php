3ed33f088e8aa1a888b8e586a062d64fbd1c19f9
Jekotia <ningleepo@gmail.com>
2012-02-26 21:08:34 -0500

    Updated sample config with new permanent and development constants.

--- sample-config.php
+++ sample-config.php
@@ -1,14 +1,36 @@
 <?php
 // Rename this file to config.php if you do not yet have one, or copy the new lines into your existing config
 
-// DB_TYPE can be 'MYSQL' or 'SQLITE'
-	define('DB_TYPE', 'MYSQL' );
-// 
-	define('DB_HOST', 'localhost' );
-	define('DB_USER', 'root');
-	define('DB_PASS', 'november' );
-	define('DB_DB', 'jephir' );
-// 
-	define('TABLE_PREFIX', 'jcf_' );
-	define('SITE_NAME', 'jephir' ); // this is temporary until I've sorted out the in-database configuration
-?>
+<?php
+
+//	The type of database to use
+//	This can be 'MYSQL' or 'SQLITE'
+define('DB_TYPE', 'MYSQL' );
+
+//	Hostname for your database server
+define('DB_HOST', 'localhost' );
+
+//	Username to connect to the database server with
+define('DB_USER', 'root');
+
+//	Password to connect to the database server with
+define('DB_PASSWORD', 'november' );
+
+//	The name of the database for Jephir
+define('DB_NAME', 'jephir' );
+
+//	The prefix to use
+define('TABLE_PREFIX', 'jcf_' );
+
+//	Database Charset to use in creating database tables.
+define('DB_CHARSET', 'utf8');
+
+//	The Database Collate type. Don't change this if in doubt.
+define('DB_COLLATE', '');
+
+//	
+define('DEBUG', false);
+
+//	these are temporary until I've sorted out the in-database configuration
+define('SITE_NAME', 'jephir' );
+define('WEB_ROOT', '/users/jekotia/jekotia.net/dev/jephir/' );
