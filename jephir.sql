ade76e24c85e26e7592ef1ffdead507ce72874cf
Jekotia <ningleepo@gmail.com>
2012-02-29 20:06:45 -0500

    Updated for latest schema.

--- jephir.sql
+++ jephir.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50516
 File Encoding         : 65001
 
-Date: 2012-02-28 23:13:27
+Date: 2012-02-29 20:05:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,6 +29,34 @@ CREATE TABLE `jcf_categories` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for `jcf_pages`
+-- ----------------------------
+DROP TABLE IF EXISTS `jcf_pages`;
+CREATE TABLE `jcf_pages` (
+  `site_id` int(11) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `nicename` varchar(255) NOT NULL,
+  `order` int(11) DEFAULT NULL,
+  `author` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `modified` timestamp NULL DEFAULT NULL,
+  `category` int(11) NOT NULL,
+  `type` varchar(32) NOT NULL,
+  `parent` int(11) NOT NULL,
+  `content` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of jcf_pages
+-- ----------------------------
+INSERT INTO `jcf_pages` VALUES ('1', '1', 'Home', 'home', '1', '1', '2012-02-29 16:38:55', null, '0', 'static', '0', 0x57656C636F6D6520746F206A657068697221205468697320434D532069732063757272656E746C7920696E2076657279206561726C792073746167657320616E6420756E6465722061637469766520646576656C6F706D656E742E20546869732075676C79207468656D6520697320707572656C7920666F7220646576656C6F706D656E7420707572706F7365732C206173206974206D616B65732069742065617369657220746F2073656520746865207061676520656C656D656E74732E);
+INSERT INTO `jcf_pages` VALUES ('1', '2', 'About', 'about', '3', '1', '2012-02-29 16:13:56', null, '0', 'static', '0', 0x4D79206E616D652069732E2E2E);
+INSERT INTO `jcf_pages` VALUES ('1', '3', 'Contact', 'contact', '4', '1', '2012-02-29 16:13:58', null, '0', 'static', '0', 0x436F6E74616374206D652061742E2E2E);
+INSERT INTO `jcf_pages` VALUES ('1', '4', 'Blog', 'blog', '2', '1', '2012-02-29 16:13:50', null, '0', 'blog', '0', 0x424C4F472E424C4F4221);
+
+-- ----------------------------
 -- Table structure for `jcf_posts`
 -- ----------------------------
 DROP TABLE IF EXISTS `jcf_posts`;
@@ -41,7 +69,6 @@ CREATE TABLE `jcf_posts` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `modified` timestamp NULL DEFAULT NULL,
   `category` int(11) NOT NULL,
-  `type` varchar(32) NOT NULL,
   `parent` int(11) NOT NULL,
   `content` blob,
   PRIMARY KEY (`id`)
@@ -50,10 +77,10 @@ CREATE TABLE `jcf_posts` (
 -- ----------------------------
 -- Records of jcf_posts
 -- ----------------------------
-INSERT INTO `jcf_posts` VALUES ('1', '1', 'Home', 'home', '1', '2012-02-26 18:31:02', null, '0', 'page', '0', 0x57656C636F6D6520746F2E2E2E);
-INSERT INTO `jcf_posts` VALUES ('1', '2', 'About', 'about', '1', '2012-02-26 18:30:52', null, '0', 'page', '0', 0x4D79206E616D652069732E2E2E);
-INSERT INTO `jcf_posts` VALUES ('1', '3', 'Contact', 'contact', '1', '2012-02-26 18:30:58', null, '0', 'page', '0', 0x436F6E74616374206D652061742E2E2E);
-INSERT INTO `jcf_posts` VALUES ('1', '4', 'Blog', 'blog', '1', '2012-02-28 23:11:12', null, '0', 'blog', '0', 0x424C4F472E424C4F4221);
+INSERT INTO `jcf_posts` VALUES ('1', '1', 'Home', 'home', '1', '2012-02-26 18:31:02', null, '0', '0', 0x57656C636F6D6520746F2E2E2E);
+INSERT INTO `jcf_posts` VALUES ('1', '2', 'About', 'about', '1', '2012-02-26 18:30:52', null, '0', '0', 0x4D79206E616D652069732E2E2E);
+INSERT INTO `jcf_posts` VALUES ('1', '3', 'Contact', 'contact', '1', '2012-02-26 18:30:58', null, '0', '0', 0x436F6E74616374206D652061742E2E2E);
+INSERT INTO `jcf_posts` VALUES ('1', '4', 'Blog', 'blog', '1', '2012-02-28 23:11:12', null, '0', '0', 0x424C4F472E424C4F4221);
 
 -- ----------------------------
 -- Table structure for `jcf_settings`
@@ -61,16 +88,20 @@ INSERT INTO `jcf_posts` VALUES ('1', '4', 'Blog', 'blog', '1', '2012-02-28 23:11
 DROP TABLE IF EXISTS `jcf_settings`;
 CREATE TABLE `jcf_settings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `site_id` int(11) NOT NULL DEFAULT '0',
   `label` varchar(64) NOT NULL,
   `value` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of jcf_settings
 -- ----------------------------
-INSERT INTO `jcf_settings` VALUES ('1', 'site_name', 'Jephir &raquo indev');
-INSERT INTO `jcf_settings` VALUES ('2', 'home_page', 'blog');
+INSERT INTO `jcf_settings` VALUES ('1', '0', 'site_name', 'Jephir :: indev');
+INSERT INTO `jcf_settings` VALUES ('2', '0', 'home_page', 'home');
+INSERT INTO `jcf_settings` VALUES ('3', '1', 'web_root', '/users/jekotia/jekotia.net/dev/jephir/');
+INSERT INTO `jcf_settings` VALUES ('4', '1', 'fs_root', 'C:\\xampp\\htdocs\\users\\jekotia\\jekotia.net\\dev\\jephir\\');
+INSERT INTO `jcf_settings` VALUES ('5', '1', 'title_divider', ' &raquo ');
 
 -- ----------------------------
 -- Table structure for `jcf_sites`
@@ -98,7 +129,10 @@ CREATE TABLE `jcf_users` (
   `password` varchar(64) NOT NULL,
   `salt` varchar(3) NOT NULL,
   `token` varchar(64) DEFAULT NULL,
-  `token_ip` varchar(15) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `group` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
@@ -106,3 +140,4 @@ CREATE TABLE `jcf_users` (
 -- ----------------------------
 -- Records of jcf_users
 -- ----------------------------
+INSERT INTO `jcf_users` VALUES ('1', 'jekotia', '', '7da00fb86ba55ca01f079fe98dfaa86de7da0ab4e7dcb7cb24e368dea3dff61e', 'b6d', null, '', '0', null, null);
